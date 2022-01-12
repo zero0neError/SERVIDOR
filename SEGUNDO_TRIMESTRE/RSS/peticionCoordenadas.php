d942f60a1b6f5c8d03859d1a7d808bc8455179e6
Guillermo Rueda Padilla <gruepad414@g.educaand.es>
2022-01-12 10:43:45 +0100

    cual carajos es la unidad del time?

--- SEGUNDO_TRIMESTRE/RSS/peticionCoordenadas.php
+++ SEGUNDO_TRIMESTRE/RSS/peticionCoordenadas.php
@@ -8,20 +8,51 @@
 </head>
 <body>
     <?php
-        if(isset($_POST['send']) && (isset($_POST['origen-calle']) && isset($_POST['origen-numero']) && isset($_POST['tipo']) && isset($_POST["origen-ciudad"]))){
+        if(isset($_POST['send']) && (isset($_POST['origen-calle']) && isset($_POST['origen-numero']) && isset($_POST['tipo']) && isset($_POST["origen-ciudad"]) && isset($_POST['destino-calle']) && isset($_POST['destino-numero']) && isset($_POST['destino-ciudad']))){
             $origen_calle=$_POST['origen-calle'];
             $origen_numero=$_POST['origen-numero'];
             $origen_ciudad=$_POST['origen-ciudad'];
-            echo "http://www.cartociudad.es/geocoder/api/geocoder/findJsonp?q={$_POST["origen-calle"]}%20{$_POST["origen-numero"]},%20{$_POST["origen-ciudad"]}&type=portal&portal={$_POST["origen-numero"]}&outputformat=geojson";
-            $data = sprintf("http://www.cartociudad.es/geocoder/api/geocoder/findJsonp?q=%s %d, %s&type=portal&portal=%d");
-            $fichero = file_get_contents("http://www.cartociudad.es/geocoder/api/geocoder/findJsonp?q={$_POST["origen-calle"]}%20{$_POST["origen-numero"]},%20{$_POST["origen-ciudad"]}&type=portal&portal={$_POST["origen-numero"]}&outputformat=geojson");
-            $fichero = substr($fichero,9,-1);
-            $datos=json_decode($fichero);
-            var_dump($datos);
-            $latitud=$datos->lat;
-            $longitud=$datos->lng;
+
+            $destino_calle=$_POST['destino-calle'];
+            $destino_numero=$_POST['destino-numero'];
+            $destino_ciudad=$_POST['destino-ciudad'];
+
+            $tipo = $_POST['tipo'];
             
-            //$distance = file_get_contents("http://www.cartociudad.es/services/api/route?orig={$_POST['orig-lng']},{$_POST['orig-lat']}&dest={$_POST['dest-long']},{$_POST['dest-lat']}&locale=es&vehicle={$_POST['tipo']}");
+
+            //ORIGEN
+            //str_replace(" ","%20",$string)
+            $data_origen = str_replace(' ', '%20', sprintf("http://www.cartociudad.es/geocoder/api/geocoder/findJsonp?q=%s %d, %s&type=portal&portal=%d",$origen_calle,$origen_numero,$origen_ciudad,$origen_numero));
+    
+            $origen_callback = file_get_contents("$data_origen");
+            $origen_callback = substr($origen_callback,9,-1);
+            $origen_datos=json_decode($origen_callback);
+            
+            $origen_latitud=$origen_datos->lat;
+            $origen_longitud=$origen_datos->lng;
+
+            //DESTINO
+
+            $data_destino = str_replace(' ', '%20', sprintf("http://www.cartociudad.es/geocoder/api/geocoder/findJsonp?q=%s %d, %s&type=portal&portal=%d",$destino_calle,$destino_numero,$destino_ciudad,$destino_numero));
+            
+            $destino_callback = file_get_contents("$data_destino");
+            $destino_callback = substr($destino_callback,9,-1);
+            $destino_datos=json_decode($destino_callback);
+            
+            $destino_latitud=$destino_datos->lat;
+            $destino_longitud=$destino_datos->lng;
+
+            //CALCULO DE DE TIEMPO
+
+            $data_time = str_replace(' ', '%20', sprintf("http://www.cartociudad.es/services/api/route?orig=%f,%f&dest=%f,%f&locale=es&vehicle=%s",$origen_latitud,$origen_longitud,$destino_latitud,$destino_longitud,$tipo));
+            $callback_time = file_get_contents("$data_time");
+            $datos_time=json_decode($callback_time);
+
+            //MUESTREO DE DATOS
+
+            echo "<p>El tiempo estimado es :$datos_time->time</p>";
+            echo sprintf("<p>La distancia estimada es de %d metros</p>",$datos_time->distance);
+            
         }
         
     ?>
